8090d4714951d1c522820a9a11cee46afc6adca2
Ilya Mikhaylov <ilya2198@yandex.ru>
2021-09-10 17:08:14 +0300

    - Changed sequence of methods in `GenresPopularityCalculationService` class
    - Added a comment on `retrieveFilmsInfo` method

--- popularity-calc/src/main/java/com/trial/popularitycalcwithtests/popularitycalc/GenresPopularityCalculationService.java
+++ popularity-calc/src/main/java/com/trial/popularitycalcwithtests/popularitycalc/GenresPopularityCalculationService.java
@@ -23,7 +23,6 @@ public class GenresPopularityCalculationService extends BaseService {
     private final String apiKeyUrl = "?api_key=72b56103e43843412a992a8d64bf96e9";
     private final String pageUrl = "&page=";
 
-
     @Autowired
     private Data calculatedData;
 
@@ -57,36 +56,29 @@ public class GenresPopularityCalculationService extends BaseService {
     }
 
 
-    public int totalNumberOfPages() {
-        return calculatedData.getTotalNumberOfPages();
+    @Async
+    public void getFilmsInfoAndUpdateData(int page) throws InterruptedException {
+
+        String filmsUrl = baseUrl + filmsInfoUrl + apiKeyUrl + pageUrl + page;
+
+        ListOfFilms listOfFilms = retrieveFilmsInfo(filmsUrl);
+
+        if (listOfFilms == null) {
+            logger.info("Failed to get page " + page + " from the server");
+            return;
+        }
+
+        synchronized (this) {
+            updatePopularityAndNumberOfFilms(listOfFilms);
+            increaseNumberOfProcessedPages();
+        }
+
+        logger.info("Processed page " + page + " on server");
+
     }
 
-    public int numberOfProcessedPages() {
-        return calculatedData.getNumberOfProcessedPages();
-    }
-
-    private void increaseNumberOfProcessedPages() {
-        calculatedData.setNumberOfProcessedPages(calculatedData.getNumberOfProcessedPages() + 1);
-    }
-
-    public boolean isValidGenreId(int genreId) {
-        return calculatedData.getGenresList().contains(genreId);
-    }
-
-    public boolean isCalculationFinished() {
-        return numberOfProcessedPages() + 1 == totalNumberOfPages();
-    }
-
-    public double popularityOfGenre(int genreId) {
-        return calculatedData.getGenreToPopularity().get(genreId) /
-                calculatedData.getGenreToNumberOfFilms().get(genreId);
-    }
-
-    public ArrayList<Integer> genresIds() {
-        return calculatedData.getGenresList();
-    }
-
-
+    // need this method because connection can be rejected
+    // due to many simultaneous connection tries to different pages on server
     private ListOfFilms retrieveFilmsInfo(String url) {
         ListOfFilms listOfFilms = null;
         int numberOfConnectionTries = 0;
@@ -115,26 +107,34 @@ public class GenresPopularityCalculationService extends BaseService {
         }
     }
 
+    private void increaseNumberOfProcessedPages() {
+        calculatedData.setNumberOfProcessedPages(calculatedData.getNumberOfProcessedPages() + 1);
+    }
 
-    @Async
-    public void getFilmsInfoAndUpdateData(int page) throws InterruptedException {
 
-        String filmsUrl = baseUrl + filmsInfoUrl + apiKeyUrl + pageUrl + page;
+    public int totalNumberOfPages() {
+        return calculatedData.getTotalNumberOfPages();
+    }
 
-        ListOfFilms listOfFilms = retrieveFilmsInfo(filmsUrl);
+    public int numberOfProcessedPages() {
+        return calculatedData.getNumberOfProcessedPages();
+    }
 
-        if (listOfFilms == null) {
-            logger.info("Failed to get page " + page + " from the server");
-            return;
-        }
+    public boolean isValidGenreId(int genreId) {
+        return calculatedData.getGenresList().contains(genreId);
+    }
 
-        synchronized (this) {
-            updatePopularityAndNumberOfFilms(listOfFilms);
-            increaseNumberOfProcessedPages();
-        }
+    public boolean isCalculationFinished() {
+        return numberOfProcessedPages() + 1 == totalNumberOfPages();
+    }
 
-        logger.info("Processed page " + page + " on server");
+    public double popularityOfGenre(int genreId) {
+        return calculatedData.getGenreToPopularity().get(genreId) /
+                calculatedData.getGenreToNumberOfFilms().get(genreId);
+    }
 
+    public ArrayList<Integer> genresIds() {
+        return calculatedData.getGenresList();
     }
 
 }
